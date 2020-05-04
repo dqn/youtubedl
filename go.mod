1d44bd322361e4621a23d1d52da2ad8cbcbf8a6d
dqn <suyarizzz@gmail.com>
2020-05-04 23:40:44 +0900

    update: split module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dqn/youtubedl
 
 go 1.14
+
+require github.com/dqn/godl v0.0.0-20200504143542-69fc0ad26718
--- main.go
+++ main.go
@@ -3,16 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
-)
 
-const mb = 1 << 20
+	"github.com/dqn/godl"
+)
 
 type PlayerResponse struct {
 	StreamingData StreamingData `json:"streamingData"`
@@ -61,25 +60,6 @@ type Microformat struct {
 	PlayerMicroformatRenderer PlayerMicroformatRenderer `json:"playerMicroformatRenderer"`
 }
 
-type Progress struct {
-	numerator   int64
-	denominator int64
-}
-
-func (p *Progress) Write(b []byte) (int, error) {
-	n := len(b)
-	p.numerator += int64(n)
-
-	fmt.Printf("\r%s", strings.Repeat(" ", 35))
-	fmt.Printf("\rdownloading... %.1f", float64(p.numerator)/mb)
-	if p.denominator > 0 {
-		fmt.Printf("/%.1f", float64(p.denominator)/mb)
-	}
-	fmt.Print(" MB")
-
-	return n, nil
-}
-
 func usage() {
 	fmt.Println("Usage: youtubedl <video id>")
 }
@@ -122,29 +102,6 @@ func findExtention(s string) string {
 	return s[strings.Index(s, "/")+1 : strings.Index(s, ";")]
 }
 
-func download(u, dest string) error {
-	resp, err := http.Get(u)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	f, err := os.Create(dest)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	w := &Progress{denominator: resp.ContentLength}
-	_, err = io.Copy(f, io.TeeReader(resp.Body, w))
-	fmt.Println()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func run() error {
 	if len(os.Args) != 2 {
 		usage()
@@ -180,7 +137,8 @@ func run() error {
 
 	ext := findExtention(highestQuality.MimeType)
 	dest := fmt.Sprintf("%s.%s", pmr.Title.SimpleText, ext)
-	if err := download(highestQuality.URL, dest); err != nil {
+
+	if err := godl.Download(highestQuality.URL, dest, true); err != nil {
 		return err
 	}
 
