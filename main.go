018b11e45d26af6451fa402c25041cf787e6690a
dqn <suyarizzz@gmail.com>
2020-05-04 15:29:37 +0900

    feat: download

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
 
 type PlayerResponse struct {
@@ -16,16 +18,13 @@ type PlayerResponse struct {
 }
 
 type AdaptiveFormat struct {
-	URL              string `json:"url"`
-	MimeType         string `json:"mimeType"`
-	Width            int    `json:"width"`
-	Height           int    `json:"height"`
-	LastModified     string `json:"lastModified"`
-	ContentLength    string `json:"contentLength"`
-	Quality          string `json:"quality"`
-	Fps              int    `json:"fps"`
-	QualityLabel     string `json:"qualityLabel"`
-	ApproxDurationMs string `json:"approxDurationMs"`
+	URL            string `json:"url"`
+	MimeType       string `json:"mimeType"`
+	Width          int    `json:"width"`
+	Height         int    `json:"height"`
+	Quality        string `json:"quality"`
+	Fps            int    `json:"fps"`
+	AverageBitrate int    `json:"averageBitrate"`
 }
 
 type StreamingData struct {
@@ -79,6 +78,24 @@ func getVideoInfo(videoID string) (url.Values, error) {
 	return v, nil
 }
 
+func printVideoInfo(videoID string, r *PlayerMicroformatRenderer, f *AdaptiveFormat) {
+	videoURL := "https://www.youtube.com/watch?v=" + videoID
+	fmt.Printf("Channel: %s (%s)\n", r.OwnerChannelName, r.OwnerProfileURL)
+	fmt.Printf("Title: %s (%s)\n", r.Title.SimpleText, videoURL)
+	fmt.Printf("Published: %s\n", r.PublishDate)
+	fmt.Printf("Length: %ss\n", r.LengthSeconds)
+	fmt.Printf("View Count: %s views\n", r.ViewCount)
+	fmt.Printf("Mime Type: %s\n", f.MimeType)
+	fmt.Printf("FPS: %dfps\n", f.Fps)
+	fmt.Printf("Size: %dx%d\n", f.Width, f.Height)
+	fmt.Printf("Quality: %s\n", f.Quality)
+	fmt.Printf("Average Bitrate: %d bps\n", f.AverageBitrate)
+}
+
+func findExtention(s string) string {
+	return s[strings.Index(s, "/")+1 : strings.Index(s, ";")]
+}
+
 func run() error {
 	if len(os.Args) != 2 {
 		usage()
@@ -101,13 +118,36 @@ func run() error {
 		return err
 	}
 
-	videoURL := "https://www.youtube.com/watch?v=" + videoID
-	pmr := p.Microformat.PlayerMicroformatRenderer
-	fmt.Printf("Channel: %s (%s)\n", pmr.OwnerChannelName, pmr.OwnerProfileURL)
-	fmt.Printf("Title: %s (%s)\n", pmr.Title.SimpleText, videoURL)
-	fmt.Printf("Published: %s\n", pmr.PublishDate)
-	fmt.Printf("Length: %ss\n", pmr.LengthSeconds)
-	fmt.Printf("View Count: %s\n", pmr.ViewCount)
+	var highestQuality AdaptiveFormat
+	for _, v := range p.StreamingData.AdaptiveFormats {
+		if v.AverageBitrate > highestQuality.AverageBitrate {
+			highestQuality = v
+		}
+	}
+
+	printVideoInfo(videoID, &p.Microformat.PlayerMicroformatRenderer, &highestQuality)
+	fmt.Println()
+	fmt.Println("downloading...")
+
+	resp, err := http.Get(highestQuality.URL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	ext := findExtention(highestQuality.MimeType)
+	dst := fmt.Sprintf("%s.%s", p.Microformat.PlayerMicroformatRenderer.Title.SimpleText, ext)
+	f, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(f, resp.Body); err != nil {
+		return err
+	}
+
+	fmt.Println("completed!")
 
 	return nil
 }
