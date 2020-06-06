58882ffcfb931d021e8a81fa93407345d687c03a
dqn <suyarizzz@gmail.com>
2020-06-07 06:45:50 +0900

    refactor: make simply

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dqn/youtubedl
 
 go 1.14
 
-require github.com/dqn/godl v0.0.0-20200504143542-69fc0ad26718
+require (
+	github.com/dqn/godl v0.0.0-20200504151700-c4604ccd729b
+	github.com/dqn/ytvi v0.0.0-20200606201819-626a010ddfd8
+)
--- main.go
+++ main.go
@@ -1,91 +1,41 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/url"
 	"os"
 	"strings"
 
 	"github.com/dqn/godl"
+	"github.com/dqn/ytvi"
 )
 
-type PlayerResponse struct {
-	StreamingData StreamingData `json:"streamingData"`
-	Microformat   Microformat   `json:"microformat"`
+func getBetween(str, a, b string) string {
+	return str[strings.Index(str, a)+1 : strings.Index(str, b)]
 }
 
-type Format struct {
-	URL          string `json:"url"`
-	MimeType     string `json:"mimeType"`
-	Width        int    `json:"width"`
-	Height       int    `json:"height"`
-	Quality      string `json:"quality"`
-	Bitrate      int    `json:"bitrate"`
-	AudioQuality string `json:"audioQuality"`
+func makeFileName(title, mime string) string {
+	ext := getBetween(mime, "/", ";")
+	return fmt.Sprintf("%s.%s", strings.ReplaceAll(title, "/", "-"), ext)
 }
 
-type StreamingData struct {
-	Formats         []Format `json:"formats"`
-	AdaptiveFormats []Format `json:"adaptiveFormats"`
-}
-
-type Title struct {
-	SimpleText string `json:"simpleText"`
-}
-
-type Description struct {
-	SimpleText string `json:"simpleText"`
-}
-
-type PlayerMicroformatRenderer struct {
-	Title            Title       `json:"title"`
-	Description      Description `json:"description"`
-	LengthSeconds    string      `json:"lengthSeconds"`
-	OwnerProfileURL  string      `json:"ownerProfileUrl"`
-	ViewCount        string      `json:"viewCount"`
-	PublishDate      string      `json:"publishDate"`
-	OwnerChannelName string      `json:"ownerChannelName"`
-}
-
-type Microformat struct {
-	PlayerMicroformatRenderer PlayerMicroformatRenderer `json:"playerMicroformatRenderer"`
-}
-
-func usage() {
-	fmt.Println("Usage:")
-	fmt.Println("  youtubedl <video id> [options]")
-	fmt.Println("Options:")
-	fmt.Println("  -m: Download as music")
-	os.Exit(2)
-}
-
-func getVideoInfo(videoID string) (url.Values, error) {
-	u := "https://www.youtube.com/get_video_info?video_id=" + videoID
-	resp, err := http.Get(u)
+func downloadMusic(videoID string) error {
+	p, err := ytvi.GetVideoInfo(videoID)
 	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
+		return err
 	}
 
-	v, err := url.ParseQuery(string(b))
-	if err != nil {
-		return nil, err
+	var f ytvi.AdaptiveFormat
+	for _, v := range p.StreamingData.AdaptiveFormats {
+		if v.AudioQuality == "" {
+			continue
+		}
+		if v.Bitrate > f.Bitrate {
+			f = v
+		}
 	}
 
-	return v, nil
-}
-
-func printCommonInfo(videoID string, r *PlayerMicroformatRenderer, f *Format) {
+	r := p.Microformat.PlayerMicroformatRenderer
 	videoURL := "https://www.youtube.com/watch?v=" + videoID
 	fmt.Printf("Channel: %s (%s)\n", r.OwnerChannelName, r.OwnerProfileURL)
 	fmt.Printf("Title: %s (%s)\n", r.Title.SimpleText, videoURL)
@@ -93,84 +43,68 @@ func printCommonInfo(videoID string, r *PlayerMicroformatRenderer, f *Format) {
 	fmt.Printf("Length: %ss\n", r.LengthSeconds)
 	fmt.Printf("View Count: %s views\n", r.ViewCount)
 	fmt.Printf("Mime Type: %s\n", f.MimeType)
+	fmt.Println()
+
+	dest := makeFileName(r.Title.SimpleText, f.MimeType)
+
+	return godl.Download(f.URL, dest, true)
 }
 
-func printMusicInfo(videoID string, r *PlayerMicroformatRenderer, f *Format) {
-	printCommonInfo(videoID, r, f)
-}
-
-func printVideoInfo(videoID string, r *PlayerMicroformatRenderer, f *Format) {
-	printCommonInfo(videoID, r, f)
-	fmt.Printf("Size: %dx%d\n", f.Width, f.Height)
-	fmt.Printf("Quality: %s\n", f.Quality)
-	fmt.Printf("Bitrate: %d bps\n", f.Bitrate)
-}
-
-func findExtention(s string) string {
-	return s[strings.Index(s, "/")+1 : strings.Index(s, ";")]
-}
-
-func run() error {
-	flag.Parse()
-
-	var (
-		videoID string
-		music   bool
-	)
-	switch flag.NArg() {
-	case 1:
-		videoID = flag.Arg(0)
-	case 2:
-		videoID = flag.Arg(0)
-		if flag.Arg(1) != "-m" {
-			usage()
-		}
-		music = true
-	default:
-		usage()
-	}
-
-	v, err := getVideoInfo(videoID)
+func downloadVideo(videoID string) error {
+	p, err := ytvi.GetVideoInfo(videoID)
 	if err != nil {
 		return err
 	}
 
-	if status, ok := v["status"]; !ok || status[0] != "ok" {
-		fmt.Println(v)
-		return fmt.Errorf("failed to get video info")
+	var f ytvi.Format
+	for _, v := range p.StreamingData.Formats {
+		if v.Bitrate > f.Bitrate {
+			f = v
+		}
 	}
 
-	var p PlayerResponse
-	if err := json.Unmarshal([]byte(v["player_response"][0]), &p); err != nil {
-		return err
-	}
-
-	pmr := p.Microformat.PlayerMicroformatRenderer
-	var highestQuality Format
-	if music {
-		for _, v := range p.StreamingData.AdaptiveFormats {
-			if v.AudioQuality == "" {
-				continue
-			}
-			if v.Bitrate > highestQuality.Bitrate {
-				highestQuality = v
-			}
-		}
-		printMusicInfo(videoID, &pmr, &highestQuality)
-	} else {
-		for _, v := range p.StreamingData.Formats {
-			if v.Bitrate > highestQuality.Bitrate {
-				highestQuality = v
-			}
-		}
-		printVideoInfo(videoID, &pmr, &highestQuality)
-	}
+	r := p.Microformat.PlayerMicroformatRenderer
+	videoURL := "https://www.youtube.com/watch?v=" + videoID
+	fmt.Printf("Channel: %s (%s)\n", r.OwnerChannelName, r.OwnerProfileURL)
+	fmt.Printf("Title: %s (%s)\n", r.Title.SimpleText, videoURL)
+	fmt.Printf("Published: %s\n", r.PublishDate)
+	fmt.Printf("Length: %ss\n", r.LengthSeconds)
+	fmt.Printf("View Count: %s views\n", r.ViewCount)
+	fmt.Printf("Mime Type: %s\n", f.MimeType)
+	fmt.Printf("Size: %dx%d\n", f.Width, f.Height)
+	fmt.Printf("Quality: %s\n", f.Quality)
+	fmt.Printf("Bitrate: %d bps\n", f.Bitrate)
 	fmt.Println()
 
-	ext := findExtention(highestQuality.MimeType)
-	dest := fmt.Sprintf("%s.%s", strings.ReplaceAll(pmr.Title.SimpleText, "/", "-"), ext)
-	if err := godl.Download(highestQuality.URL, dest, true); err != nil {
-		return err
+	dest := makeFileName(r.Title.SimpleText, f.MimeType)
+
+	return godl.Download(f.URL, dest, true)
+}
+
+func run() error {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage:\n  youtubedl [options...] <video-id>\nOptions:")
+		flag.PrintDefaults()
+	}
+	m := flag.Bool("m", false, "Download `music` only")
+
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	videoID := flag.Arg(0)
+
+	if *m {
+		if err := downloadMusic(videoID); err != nil {
+			return err
+		}
+	} else {
+		if err := downloadVideo(videoID); err != nil {
+			return err
+		}
 	}
 
 	fmt.Println("completed!")
@@ -179,8 +113,7 @@ func run() error {
 }
 
 func main() {
-	err := run()
-	if err != nil {
-		log.Fatal(err)
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 	}
 }
